9474e1f0b4d7aa5663558a946bd9196759142bb8
thshurka <114878213+thshurka@users.noreply.github.com>
2022-12-29 12:43:26 +0500

    Комментарии

--- LABS/LAB2.py
+++ LABS/LAB2.py
@@ -1,17 +1,36 @@
-alph="abcdefghljklmnopqrstuvwxyzabcdefghljklmnopqrstuvwxyz"   ###В шифре замыкается алфавит
+alph = "abcdefghljklmnopqrstuvwxyzabcdefghljklmnopqrstuvwxyz"  ###В шифре замыкается алфавит
 
-a,b = {},{}                                                   ###Создадим словари с алфавитом и номером символов
+a, b = {}, {}  ###Создадим словари с алфавитом и номером символов
 
-for i in range(len(alph)):                                    ###Заполним их так, чтобы в одном ключи были - символы, в другом - их номера
+for i in range(len(alph)):  ###Заполним их так, чтобы в одном ключи были - символы, в другом - их номера
     ch = alph[i]
-    a[ch]=i
+    a[ch] = i
 for i in range(len(alph)):
-    b[i]=alph[i]
+    b[i] = alph[i]
 
-code = int(input())
-stroke = input()
-decode=""
-for i in stroke:
-    decode+=b[a[i]+code]
 
-print(decode)
+def ce():  # обработчик ошибки ввода сдвига
+    while True:
+        try:
+            code = int(input("Введите сдвиг шифра числом "))
+            return code
+        except ValueError:
+            print("Нужно ввести цифру")
+
+
+def st():  # обработчик ошибки ввода строки
+    while True:
+        st = input("Введите строку для дешифрования ")
+        if st.isdigit() == 1:
+            print("Введите строку")
+        else:
+            return st
+
+
+decode = ""
+cd = ce()
+
+for i in st(): #дешифрование
+    decode += b[a[i] + cd]
+
+print(decode) #вывод
